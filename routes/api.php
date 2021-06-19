668aec79af5d58f590765a2210508fc2d2043589
mfsiat <nasirul.islam@northsouth.edu>
2021-06-20 02:39:52 +0600

    add model, migrate, sqlite, install dependencites sqlite3, php-sqlite3

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/products', function(){
-    return 'products';
+    return Product::all();
 }); 
+
+Route::post('/products', function(){
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'product-one',
+        'description' => 'This is product one',
+        'price' => '99.99'
+    ]);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
