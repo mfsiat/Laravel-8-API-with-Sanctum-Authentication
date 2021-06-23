7a85bcc740be2849a21da6736db619e3a2af0cab
mfsiat <nasirul.islam@northsouth.edu>
2021-06-24 01:37:24 +0600

    add log out

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,8 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class, 'store']);    
     Route::put('/products/{id}', [ProductController::class, 'update']);    
     Route::post('/products/{id}', [ProductController::class, 'destroy']);    
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
